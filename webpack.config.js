c29f8617c90fc4914dcc7c0b16f3b9084d410e40
Vishavpreet Sandhu <vishal1949@gmail.com>
2019-04-28 13:33:06 -0700

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*", '.css'],
+  }
+};
+
+
